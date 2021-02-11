74b8a3c1b56806bdd706dbdee39fe67e20d81759
cretumarius <marius.cretu@cegeka.com>
2021-02-11 19:33:46 +0200

    App rename

--- client/ios/DocScanTests/DocScanTests.m
+++ client/ios/DocScanTests/DocScanTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BudgetMonitorTests : XCTestCase
+@interface DocScanTests : XCTestCase
 
 @end
 
-@implementation BudgetMonitorTests
+@implementation DocScanTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
